15a3c96a7a9ddf8d68e3d17b8bd600d7a2a16b26
ZeR0-N <tenppppp@hotmail.com>
2023-11-07 23:42:44 +0700

    agasgagasg

--- lua/weapons/mg_base/modules/attachmentss/t9standard/barrels/attachment_vm_ar_t9standard_barrel_02_short.lua
+++ lua/weapons/mg_base/modules/attachmentss/t9standard/barrels/attachment_vm_ar_t9standard_barrel_02_short.lua
@@ -1,6 +1,6 @@
 ATTACHMENT.Base = "att_barrel"
 ATTACHMENT.Name = "13.5'' Reinforced Heavy"
-ATTACHMENT.Model = Model("models/kyo/attachment_vm_ar_t9standard_barrel_02.mdl")
+ATTACHMENT.Model = Model("models/kyo/attachment_vm_ar_t9standard_barrel_02_LOD0_short.mdl")
 local BaseClass = GetAttachmentBaseClass(ATTACHMENT.Base)
 function ATTACHMENT:Stats(weapon)
     BaseClass.Stats(self, weapon)
